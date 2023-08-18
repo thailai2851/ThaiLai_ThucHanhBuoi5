552d4703b8eeee28da7e42d2cfdff1c553c08287
thailai2851 <thailai2851@gmail.com>
2023-08-18 16:03:02 +0700

    first commit

---
+++ Bai3/lib/constants.dart
@@ -0,0 +1,10 @@
+// ignore_for_file: constant_identifier_names
+
+import 'package:flutter/material.dart';
+
+const String google_api_key = "AIzaSyCALRe0CjSLfD2RHL08DCLaIQ7pKOz8UmI";
+const Color primaryColor = Color(0xFF7B61FF);
+const double defaultPadding = 16.0;
+
+// AIzaSyAAtoqa_NliIr76Iur7QBsEqxbJMUO24Xk
+// AIzaSyDYhdyFGsrgM8eiQD0Vwzjw4C16Twp5_oI
